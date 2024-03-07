b636611a811bfaf092ade91a490f2129374b016c
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-03-07 12:17:29 +0100

    first commit

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import './Feature.css'
+import { MdOutlineLocalShipping } from "react-icons/md";
+import { MdSupportAgent } from "react-icons/md";
 
 const Feature = () => {
   return (
-    <div>Feature</div>
+    <section className='f-container'>
+      <div className='first-column'>
+        <h2>Discover the Best Deals on Phones at Our Online Store</h2>
+        <span className='first-span'>Get accss to the free shipping, 24/7 customer support, 
+          and exclusive member discounts.
+        </span>
+
+        <div className='text'>
+          <div className='text-con'>
+            <span>Free Shipping<MdOutlineLocalShipping className='ship-icon' size={40} color='rgb(150, 140, 140)'/></span>
+            <span>Enjoy the convenience of having your 
+              new phone delivered to your doorstep for free.
+            </span>
+          </div>
+
+          <div className='text-con'>
+            <span>24/7 Support<MdSupportAgent className='support-icon' size={40} color='rgb(150, 140, 140)'/></span>
+            <span>Our dedicated support team is available round
+              the clock to assist you.
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className='second-column'>
+        <img src='feature-img.jpg' alt='featureimg' />
+      </div>
+    </section>
   )
 }
 
