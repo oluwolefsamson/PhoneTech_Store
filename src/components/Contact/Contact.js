346c60cde8cf2cd330105bf049fe3f4780ec54ef
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-03-21 21:11:25 +0100

    first commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -33,10 +33,10 @@ export default function Contact() {
 
     emailjs
       .sendForm(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+         "service_dr960kp",
+        "template_e9qhjxn",
       form.current, {
-        publicKey: process.env.REACT_APP_EMAILJS_PUBLIC_KEY,
+        publicKey: "Wty5bekIHlK31t-gV",
       })
       .then(
         () => {
