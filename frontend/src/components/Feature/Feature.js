be656b3046ac9c59974da85c39dd3c6c39e88114
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-03-21 19:53:20 +0100

    first commit

---
+++ frontend/src/components/Feature/Feature.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Feature.css'
+import { MdOutlineLocalShipping } from "react-icons/md";
+import { MdSupportAgent } from "react-icons/md";
+
+const Feature = () => {
+  return (
+    <section className='f-container'>
+      <div className='first-column'>
+        <h2>Discover the Best Deals on Phones at Our Online Store</h2>
+        <span className='first-span'>Get access to the free shipping, 24/7 customer support, 
+          and exclusive member discounts.
+        </span>
+
+        <div className='text'>
+          <div className='text-con'>
+            <span>Free Shipping<MdOutlineLocalShipping  size={25} color='#141630'/></span>
+            <span>Enjoy the convenience of having your 
+              new phone delivered to your doorstep for free.
+            </span>
+          </div>
+
+          <div className='text-con'>
+            <span>24/7 Support<MdSupportAgent  size={25} color='#141630'/></span>
+            <span>Our dedicated support team is available round
+              the clock to assist you. We are always at your Service.
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className='second-column'>
+        <img src='feature-img.jpg' alt='featureimg' />
+      </div>
+    </section>
+  )
+}
+
+export default Feature
---
+++ frontend/src/components/Team/Team.js
@@ -0,0 +1,82 @@
+import React from "react"
+
+const people = [
+
+    {
+        name: 'Balogun Jimmy',
+        role: 'Co-Founder / CEO',
+        imageUrl:
+          'profile4.jpg',
+      },
+
+      {
+        name: 'Prince Willi',
+        role: 'HR',
+        imageUrl:
+          'profile3.jpg',
+      },
+
+      {
+        name: 'Ola Gold',
+        role: 'Accountant',
+        imageUrl:
+          'profile2.jpg',
+      },
+
+      {
+        name: 'Akin James',
+        role: 'Marketer',
+        imageUrl:
+          'profile5.jpg',
+      },
+
+      {
+        name: 'Michael King',
+        role: 'I.T',
+        imageUrl:
+          'profile1.jpg',
+      },
+
+      {
+        name: 'Oluwole Samson',
+        role: 'Software Developer',
+        imageUrl:
+          'profile7.jpeg',
+      },
+    // More people...
+  ]
+  
+  export default function Team() {
+    return (
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+          <div className="max-w-2xl">
+            <h2 
+            data-aos="flip-left"
+            className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our Team</h2>
+            <p 
+            data-aos="fade-up"
+            data-aos-duration="3000"
+            className="mt-6 text-lg leading-8 text-gray-600">
+                At PhoneTech, we believe that strong leadership is the cornerstone of our success. Meet the visionaries and strategists who drive our 
+                company forward, shaping the future of mobile technology and leading us towards new horizons
+            </p>
+          </div>
+          <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+            {people.map((person) => (
+              <li key={person.name}>
+                <div className="flex items-center gap-x-6">
+                  <img className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" />
+                  <div>
+                    <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
+                    <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+  
